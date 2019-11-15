cf688dec3dc997e7e0c34be431621e56afbd95f4
Quoc <nguyencongquoc147@yahoo.com.vn>
2019-11-15 10:08:42 +0700

    Nhom  THC - Nguyen Cong Quoc - Vu Van Thoi - Ngo Tuan Trung

---
+++ THC/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bai tap 2 - Nhom THC :https://github.com/betonamu/THC.git
+        }
+    }
+}
